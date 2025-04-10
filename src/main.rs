08588cce7121d5e7710d959ad2033e66e6f29007
Daniel C. Rodrigues <daniel.rodrighes@gmail.com>
2025-04-10 08:44:00 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{App, HttpServer};
+use dotenv::dotenv;
+use std::env;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let server_url = format!("{}:{}", host, port);
+
+    HttpServer::new(move || App::new())
+        .bind(server_url)?
+        .run()
+        .await
+}
